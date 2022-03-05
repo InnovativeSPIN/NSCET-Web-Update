d7011d30ee8a3a485be5f2ce63813a27861ee42d
MANOJ KUMAR <smk11500@gmail.com>
2022-03-05 23:35:01 +0530

    header & footer completed

---
+++ footer.php
@@ -0,0 +1,62 @@
+<?php ?>
+<link rel="stylesheet" href="/assets/css/footer.css">
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 item">
+                <h6>CONTACT</h6>
+                <ul>
+                    <li><a href="mailto:principal@nscet.org">principal@nscet.org</a></li>
+                    <li>04546-263900, 901, 902</li>
+                    <li>Nadar Saraswathi College of Engineering<br>and Technology,<br>PostboxNo:60,&nbsp;Annanji(P.O),<br>Vadapudupatti,&nbsp;Theni-625531</li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-12 item">
+                <h6>DETAILS</h6>
+                <ul>
+                    <li><a href="#">Approval from statutory bodies</a></li>
+                    <li><a href="#">Fee structure</a></li>
+                    <li><a href="#">Anti-Ragging</a></li>
+                    <li><a href="#">Grievance Redressal</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">Newsletter &amp; Magazines</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">CSE</a>
+                            <a class="dropdown-item" href="#">CIVIL</a>
+                            <a class="dropdown-item" href="#">MECHANICAL</a>
+                            <a class="dropdown-item" href="#">ECE</a>
+                            <a class="dropdown-item" href="#">EEE</a>
+                            <a class="dropdown-item" href="#">S&H</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-12 item">
+                <h6>DISCLOSURE</h6>
+                <ul>
+                    <li><a target="_blank" href="#">Mandatory Disclosure</a></li>
+                    <li><a href="#">Minimum Disclosure</a></li>
+                    <li><a href="#">Student Facilitation services</a></li>
+                    <li><a href="#">AICTE Approval Handbook</a></li>
+                    <li><a href="#">AU Affiliation</a></li>
+                    <li><a href="#">NIRF 2021</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-12 item">
+                <h6>Social Media</h6>
+                <ul class="social_links d-flex flex-lg-column justify-content-around">
+                    <li><a href="" target="_blank"><i class="bi bi-youtube"></i></a></li>
+                    <li><a href="" target="_blank"><i class="bi bi-facebook"></i></a></li>
+                    <li><a href="" target="_blank"><i class="bi bi-twitter"></i></a></li>
+                    <li><a href="" target="_blank"><i class="bi bi-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="copyright d-lg-flex justify-content-around text-white">
+        <p><a href="#">Dept of Computer Science &amp; Engg</a></p>
+        <p>&copy; Copyrights-2022</p>
+        <p><a href="#">innovative SPIN</a></p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,83 @@
+<?php ?>
+<link rel="stylesheet" href="/assets/css/header.css">
+
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="navigation container-fluid">
+        <a class="d-lg-flex align-items-lg-center clg-home-logo" href="../index.html">
+                <h1 class="clg-name">NSCET</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
+            <div class="d-flex flex-lg-column flex-md-row justify-content-around">
+                <ul class="primary-navi navbar-nav float-left">
+                    <li class="nav-item"><a class="nav-link" href="../index.php" >HOME</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">ADMINISTRATION</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">ADMISSION</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href='#'>Eligibility for Admission&nbsp;</a>
+                            <a class="dropdown-item" href='#'>B.E Admission</a>
+                            <a class="dropdown-item" href='#'>M.E / M.Tech Admission</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">ACADEMICS</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">UG Programmes</a>
+                            <a class="dropdown-item" href="#">PG Programmes</a>
+                            <a class="dropdown-item" href="#">Regulations</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">DEPARTMENTS</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Civil Engineering</a>
+                                <a class="dropdown-item" href="#">Computer Science &amp; Engineering</a>
+                                <a class="dropdown-item" href="#">Electronics &amp; Communication Engineering</a>
+                                <a class="dropdown-item" href="#">Electrical and Electronics Engineering</a>
+                                <a class="dropdown-item" href="#">Mechanical Engineering</a>
+                                <a class="dropdown-item" href="#">Science and Humanities</a>
+                        </div>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">HOSTEL</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PLACEMENT</a>                       
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">PORTAL</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Staff</a>
+                            <a class="dropdown-item" href="#">Student</a>
+                        </div>                           
+                    </li>
+                </ul>
+
+                <ul class="secondry-navi navbar-nav d-flex">
+                    <li class="nav-item"><a class="nav-link" href="#">Culturals</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Library</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Infrastructure</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" >Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" >Careers</a></li> 
+                    <li class="nav-item"><a class="nav-link" href="#">Alumni</a></li>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">Clubs &amp; Chapters</a>
+                        <div class="dropdown-menu"><a class="dropdown-item" href="#">IIPC</a><a class="dropdown-item" href="#">ISTE Student Chapter</a>
+                            <a class="dropdown-item" href='#'>Clubs</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown">NIRF</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item"  href="../department/assets/Docs/nirf/NADAR SARASWATHI COLLEGE OF ENGINEERING & TECHNOLOGY20210628-(1).pdf">2021</a>
+                            <a class="dropdown-item"  href="../department/assets/Docs/nirf/NADAR SARASWATHI COLLEGE OF ENGINEERING & TECHNOLOGY20220204.pdf">2022</a>
+                        </div>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="../cells/index.html#item-1-19">IQAC</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../research/index.html">Research</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+</header>
